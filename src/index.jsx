dca9c6185fe3c7b1279958b3c6aed75c912c3873
Mark Panaghiston <markp@happyworm.com>
2016-11-19 14:25:13 +0000

    Added RichUtils & Key commands

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Editor, EditorState } from 'draft-js';
+import { Editor, EditorState, RichUtils } from 'draft-js';
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = { editorState: EditorState.createEmpty() };
     this.onChange = (editorState) => { this.setState({ editorState }); };
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return 'handled';
+    }
+    return 'not-handled';
   }
   render() {
-    const { editorState } = this.state;
-    return <Editor editorState={editorState} onChange={this.onChange} />;
+    return (
+      <Editor
+        editorState={this.state.editorState}
+        handleKeyCommand={this.handleKeyCommand}
+        onChange={this.onChange}
+      />
+    );
   }
 }
 
